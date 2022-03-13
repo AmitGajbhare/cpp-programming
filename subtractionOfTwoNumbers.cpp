983f58c17f6e90f638363c34732b9adf84ccc651
Amit Gajbhare <71889165+AmitGajbhare@users.noreply.github.com>
2022-03-13 20:08:58 +0530

    subtractionOfTwoNumbers.cpp is added

---
+++ subtractionOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+// subtraction of two numbers
+// Author name - AmitGajbhare
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter a two numbers: ";
+    cin>>a>>b;
+    c=a-b;
+    cout<<"The Subtraction of two numbers is: "<<c;
+
+    return 0;
+}
