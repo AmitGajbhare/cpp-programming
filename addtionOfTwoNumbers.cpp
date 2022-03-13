16e0fa23d6450139025cc5a955b5a57107456974
Amit Gajbhare <71889165+AmitGajbhare@users.noreply.github.com>
2022-03-13 20:00:26 +0530

    addtionOfTwoNumbers.cpp is added

---
+++ addtionOfTwoNumbers.cpp
@@ -0,0 +1,17 @@
+// Addition of two numbers
+// Author AmitGajbhare
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter a two numbers for addions: ";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"The addtion of two numbers is: "<<c<<endl;
+
+    return 0;
+}
+
+
